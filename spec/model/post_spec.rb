a3a57dd96c1d9e928998f0f1fcaafe58e9a50f05
Samuel-Ricardo <samuelricardoofficial@gmail.com>
2022-10-16 15:45:46 -0300

    :white_check_mark: create: test -  title must be valit [post]

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Post, :type => :model do
   context "Post title valitation" do
     
+    it "must be valid" do
+      post = Post.new(title: "First Post", content: 'Nice Content :D')
+      expect(post).to be_valid
+    end
+    
 
-  
   end
 end
